1ca2936045bba86efe1d3f7c481984944a046111
Your Name <you@example.com>
2021-04-27 18:36:09 +0700

    update users

--- AutoWorld/Areas/Admin/Controllers/UsersController.cs
+++ AutoWorld/Areas/Admin/Controllers/UsersController.cs
@@ -4,13 +4,138 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using AutoWorld.Common;
 using AutoWorld.Models;
+using AutoWorld.Models.ViewModels;
 
 namespace AutoWorld.Areas.Admin.Controllers
 {
+    //[Authorize(Roles = "admin")]
+    //[CustomActionFilter]
+    //[ExceptionHandlerFilter]
+    //public class UsersController : Controller
+    //{
+    //    private AutoWorlDbContext db = new AutoWorlDbContext();
+
+    //    // GET: Admin/Users
+    //    public ActionResult Index()
+    //    {
+    //        var users = db.Users.Include(u => u.Accounts);
+    //        return View(users.ToList());
+    //    }
+
+    //    // GET: Admin/Users/Details/5
+    //    public ActionResult Details(long? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        Users users = db.Users.Find(id);
+    //        if (users == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(users);
+    //    }
+
+    //    // GET: Admin/Users/Create
+    //    public ActionResult Create()
+    //    {
+    //        ViewBag.AccountId = new SelectList(db.Accounts, "Id", "LoginName");
+    //        return View();
+    //    }
+
+    //    // POST: Admin/Users/Create
+    //    // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+    //    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Create([Bind(Include = "Id,DisplayName,Roles,AccountId")] Users users)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            db.Users.Add(users);
+    //            db.SaveChanges();
+    //            return RedirectToAction("Index");
+    //        }
+
+    //        ViewBag.AccountId = new SelectList(db.Accounts, "Id", "LoginName", users.AccountId);
+    //        return View(users);
+    //    }
+
+    //    // GET: Admin/Users/Edit/5
+    //    public ActionResult Edit(long? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        Users users = db.Users.Find(id);
+    //        if (users == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        ViewBag.AccountId = new SelectList(db.Accounts, "Id", "LoginName", users.AccountId);
+    //        return View(users);
+    //    }
+
+    //    // POST: Admin/Users/Edit/5
+    //    // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+    //    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Edit([Bind(Include = "Id,DisplayName,Roles,AccountId")] Users users)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            db.Entry(users).State = EntityState.Modified;
+    //            db.SaveChanges();
+    //            return RedirectToAction("Index");
+    //        }
+    //        ViewBag.AccountId = new SelectList(db.Accounts, "Id", "LoginName", users.AccountId);
+    //        return View(users);
+    //    }
+
+    //    // GET: Admin/Users/Delete/5
+    //    public ActionResult Delete(long? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //        }
+    //        Users users = db.Users.Find(id);
+    //        if (users == null)
+    //        {
+    //            return HttpNotFound();
+    //        }
+    //        return View(users);
+    //    }
+
+    //    // POST: Admin/Users/Delete/5
+    //    [HttpPost, ActionName("Delete")]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult DeleteConfirmed(long id)
+    //    {
+    //        Users users = db.Users.Find(id);
+    //        db.Users.Remove(users);
+    //        db.SaveChanges();
+    //        return RedirectToAction("Index");
+    //    }
+
+    //    protected override void Dispose(bool disposing)
+    //    {
+    //        if (disposing)
+    //        {
+    //            db.Dispose();
+    //        }
+    //        base.Dispose(disposing);
+    //    }
+    //}
+
     [Authorize(Roles = "admin")]
     [CustomActionFilter]
     [ExceptionHandlerFilter]
@@ -19,31 +144,31 @@ namespace AutoWorld.Areas.Admin.Controllers
         private AutoWorlDbContext db = new AutoWorlDbContext();
 
         // GET: Admin/Users
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             var users = db.Users.Include(u => u.Accounts);
-            return View(users.ToList());
+            return View(await users.ToListAsync());
         }
 
         // GET: Admin/Users/Details/5
-        public ActionResult Details(long? id)
+        public async Task<ActionResult> Details(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Users users = db.Users.Find(id);
-            if (users == null)
+            Users user = await db.Users.FindAsync(id);
+            if (user == null)
             {
                 return HttpNotFound();
             }
-            return View(users);
+            return View(user);
         }
 
         // GET: Admin/Users/Create
         public ActionResult Create()
         {
-            ViewBag.AccountId = new SelectList(db.Accounts, "Id", "LoginName");
+            ViewBag.RolesList = new SelectList(DataUtils.GetRolesList(), "Key", "Value");
             return View();
         }
 
@@ -52,33 +177,36 @@ namespace AutoWorld.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,DisplayName,Roles,AccountId")] Users users)
+        public async Task<ActionResult> Create(UserAccountView viewModel)
         {
             if (ModelState.IsValid)
             {
-                db.Users.Add(users);
-                db.SaveChanges();
+                Users user = new Users();
+                viewModel.CopyToUser(ref user);
+                db.Users.Add(user);
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
+            ViewBag.RolesList = new SelectList(DataUtils.GetRolesList(), "Key", "Value", viewModel.Roles);
 
-            ViewBag.AccountId = new SelectList(db.Accounts, "Id", "LoginName", users.AccountId);
-            return View(users);
+            return View(viewModel);
         }
 
         // GET: Admin/Users/Edit/5
-        public ActionResult Edit(long? id)
+        public async Task<ActionResult> Edit(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Users users = db.Users.Find(id);
-            if (users == null)
+            Users user = await db.Users.FindAsync(id);
+            if (user == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.AccountId = new SelectList(db.Accounts, "Id", "LoginName", users.AccountId);
-            return View(users);
+            UserAccountView viewModel = new UserAccountView(user);
+            ViewBag.RolesList = new SelectList(DataUtils.GetRolesList(), "Key", "Value", viewModel.Roles);
+            return View(viewModel);
         }
 
         // POST: Admin/Users/Edit/5
@@ -86,41 +214,44 @@ namespace AutoWorld.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,DisplayName,Roles,AccountId")] Users users)
+        public async Task<ActionResult> Edit(UserAccountView viewModel)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(users).State = EntityState.Modified;
-                db.SaveChanges();
+                Users user = await db.Users.FindAsync(viewModel.Id);
+                viewModel.CopyToUser(ref user); // TODO: prevent update username and password
+                db.Entry(user).State = EntityState.Modified;
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            ViewBag.AccountId = new SelectList(db.Accounts, "Id", "LoginName", users.AccountId);
-            return View(users);
+            ViewBag.RolesList = new SelectList(DataUtils.GetRolesList(), "Key", "Value", viewModel.Roles);
+
+            return View(viewModel);
         }
 
         // GET: Admin/Users/Delete/5
-        public ActionResult Delete(long? id)
+        public async Task<ActionResult> Delete(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Users users = db.Users.Find(id);
-            if (users == null)
+            Users user = await db.Users.FindAsync(id);
+            if (user == null)
             {
                 return HttpNotFound();
             }
-            return View(users);
+            return View(user);
         }
 
         // POST: Admin/Users/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(long id)
+        public async Task<ActionResult> DeleteConfirmed(long id)
         {
-            Users users = db.Users.Find(id);
-            db.Users.Remove(users);
-            db.SaveChanges();
+            Users user = await db.Users.FindAsync(id);
+            db.Users.Remove(user);
+            await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 

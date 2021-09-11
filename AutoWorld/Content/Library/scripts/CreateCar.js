d1eb8c7ba5dbcc8156d5aaf4486afcb37a6cec66
quydang1412 <quydang1412@gmail.com>
2021-09-11 09:56:17 +0700

    Edit form CreateCar

---
+++ AutoWorld/Content/Library/scripts/CreateCar.js
@@ -0,0 +1,27 @@
+﻿////$('.btn-image').on("click", function (e) {
+////    e.preventDefault();
+////    var finder = new CKFinder();
+////    finder.selectActionFunction = function (url) {
+////        //$('#txtImage').val(url);
+////        $('.txtImg').val(url);
+
+////    };
+////    finder.popup();
+////});
+
+var selectUrlForImage = function (btnselect, txtImg) {
+    btnselect.on("click", function (e) {
+        e.preventDefault();
+        var finder = new CKFinder();
+        finder.selectActionFunction = function (url) {
+            txtImg.val(url);
+
+        };
+        finder.popup();
+    });
+}
+
+selectUrlForImage($('#btnSelectImage'), $('#txtImage'));
+selectUrlForImage($('#btnSelectImageDetail'), $('#txtImageDetail'));
+selectUrlForImage($('#btnSelectImageDetail1'), $('#txtImageDetail1'));
+selectUrlForImage($('#btnSelectImageDetail2'), $('#txtImageDetail2'));
---
+++ AutoWorld/Models/ViewModels/CarView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutoWorld.Models.ViewModels
+{
+    public class CarView
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Image { get; set; }
+
+        public long CategoryId { get; set; }
+
+        public double Price { get; set; }
+
+        public long Seat { get; set; }
+
+        public string Model { get; set; }
+
+        public string Fuel{ get; set; }
+
+        public string Origin { get; set; }
+
+        public string Other { get; set; }
+
+        public string ImageDetail { get; set; }
+
+        public string ImageDetail1 { get; set; }
+
+        public string ImageDetail2 { get; set; }
+
+        [StringLength(1024)]
+        public string Description { get; set; }
+
+        [StringLength(1024)]
+        public string Content { get; set; }
+
+        public long? LocationId { get; set; }
+
+    }
+}

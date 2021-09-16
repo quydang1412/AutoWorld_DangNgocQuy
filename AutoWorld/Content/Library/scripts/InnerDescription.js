37656c081f5a77e51f132d0ad7d4923178bd83b8
quydang1412 <quydang1412@gmail.com>
2021-09-16 11:42:41 +0700

    add method change Image for img element

--- AutoWorld/Content/Library/scripts/InnerDescription.js
+++ AutoWorld/Content/Library/scripts/InnerDescription.js
@@ -4,13 +4,14 @@ var stringInnerDiscription = '';
 var m = 0;
 var srcImageDetail = '';
 
-var selectUrlForImage = function (btnselect, txtImg) {
+var selectUrlForImage = function (btnselect, txtImg, imgDetail) {
     btnselect.on("click", function (e) {
         e.preventDefault();
         var finder = new CKFinder();
         finder.selectActionFunction = function (url) {
             txtImg.val(url);
-
+            var srcLink = "/Content/Library/" + url;
+            imgDetail.attr('src', srcLink);
         };
         finder.popup();
     });
@@ -20,8 +21,8 @@ var selectUrlForImage = function (btnselect, txtImg) {
 jQuery.each(objectDescription, function (k, value) {
     if (k.includes("img")) {
         srcImageDetail = "/Content/Library/" + value; 
-        console.log(srcImageDetail);
-        stringInnerDiscription += '<div class="form-group"><label class="control-label col-md-2">' + k + '</label><div class="col-md-10"><input class="form-control" type="text" id="txtImageDetail' + m + '" value="' + value + '" /><a href="#" type="button" class="btn btn-success btn-image" id="btnSelectImageDetail' + m + '">Select</a></div><div class="pt-1 pl-3"><img class="imgSize" src="' + srcImageDetail+'"/></div></div>';
+        
+        stringInnerDiscription += '<div class="form-group"><label class="control-label col-md-2">' + k + '</label><div class="col-md-10"><input class="form-control" type="text" id="txtImageDetail' + m + '" value="' + value + '" /><a href="#" type="button" class="btn btn-success btn-image" id="btnSelectImageDetail' + m + '">Select</a></div><div class="pt-1 pl-3"><img class="imgSize" id="imgDetail'+m+'" src="' + srcImageDetail+'"/></div></div>';
         m++;
 
     } else {
@@ -31,15 +32,32 @@ jQuery.each(objectDescription, function (k, value) {
     
 });
 
+
+//var ChangeImage = function (i) {
+//    var i = 0;
+//    var txtSelect = '#txtImageDetail' + i;
+//    var imgDetail = '#imgDetail' + i;
+//    var srcImg = '/Content/Library/' + $(txtSelect).val();
+//    $(imgDetail).attr('src', srcImg);
+//    //$(imgDetail).attr('src') = '/Content/Library /' + $(txtSelect).val();
+//    //console.log($(imgDetail).attr('src'));
+//    //console.log($(txtSelect).val());
+
+
+//};
+
 $('#InnerDiscription').html(stringInnerDiscription);
 
 for (var i = 0; i < m; i++) {
         var btnSelect = '#btnSelectImageDetail' + i;
-        var txtSelect = '#txtImageDetail' + i;
-    selectUrlForImage($(btnSelect), $(txtSelect));
+    var txtSelect = '#txtImageDetail' + i;
+    var imgDetail = '#imgDetail' + i;
+    selectUrlForImage($(btnSelect), $(txtSelect), $(imgDetail));
     
 }
-selectUrlForImage($('#btnSelectImage'), $('#txtImage'));
+selectUrlForImage($('#btnSelectImage'), $('#txtImage'), $('#imgDisplay'));
+
+
 
 
 

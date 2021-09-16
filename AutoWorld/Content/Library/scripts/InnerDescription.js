22284d0463c657a7071fdf4c0c0fb7a800845645
quydang1412 <quydang1412@gmail.com>
2021-09-16 07:50:37 +0700

    edit images resize

--- AutoWorld/Content/Library/scripts/InnerDescription.js
+++ AutoWorld/Content/Library/scripts/InnerDescription.js
@@ -1,22 +1,29 @@
 ﻿var DetailDescription = $('#DetailDescription').val();
 var objectDescription = JSON.parse(DetailDescription);
 var stringInnerDiscription = '';
-//Object.entries(DetailDescription).forEach(([value,key]) => {
-//    stringInnerDiscription += '<label>' + `${key}` + '</label><input type="text" value="' + `${value}`+'" />"'
-
-    
-//});
-
-//for (var k in DetailDescription) {
-
-//    stringInnerDiscription += '<label>' + k + '</label><input type="text" value="' + DetailDescription[k] + '" />"'
-//}
 var m = 0;
+var srcImageDetail = '';
+
+var selectUrlForImage = function (btnselect, txtImg) {
+    btnselect.on("click", function (e) {
+        e.preventDefault();
+        var finder = new CKFinder();
+        finder.selectActionFunction = function (url) {
+            txtImg.val(url);
+
+        };
+        finder.popup();
+    });
+}
+
 
 jQuery.each(objectDescription, function (k, value) {
     if (k.includes("img")) {
-        stringInnerDiscription += '<div class="form-group"><label class="control-label col-md-2">' + k + '</label><div class="col-md-10"><input class="form-control" type="text" value="' + value + '" /><a href="#" type="button" class="btn btn-success btn-image" id="btnSelectImage' + m +'">Select</a></div></div>';
+        srcImageDetail = "/Content/Library/" + value; 
+        console.log(srcImageDetail);
+        stringInnerDiscription += '<div class="form-group"><label class="control-label col-md-2">' + k + '</label><div class="col-md-10"><input class="form-control" type="text" id="txtImageDetail' + m + '" value="' + value + '" /><a href="#" type="button" class="btn btn-success btn-image" id="btnSelectImageDetail' + m + '">Select</a></div><div class="pt-1 pl-3"><img class="imgSize" src="' + srcImageDetail+'"/></div></div>';
         m++;
+
     } else {
         stringInnerDiscription += '<div class="form-group"><label class="control-label col-md-2">' + k + '</label><div class="col-md-10"><input class="form-control" type="text" value="' + value + '" /></div></div>'
     }
@@ -24,7 +31,23 @@ jQuery.each(objectDescription, function (k, value) {
     
 });
 
-
-
 $('#InnerDiscription').html(stringInnerDiscription);
+
+for (var i = 0; i < m; i++) {
+        var btnSelect = '#btnSelectImageDetail' + i;
+        var txtSelect = '#txtImageDetail' + i;
+    selectUrlForImage($(btnSelect), $(txtSelect));
+    
+}
+selectUrlForImage($('#btnSelectImage'), $('#txtImage'));
+
+
+
+
+
+
+
+
+
+
 

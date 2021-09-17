76fffc26e16d1a5c824509f9909ff7086daebdc9
quydang1412 <quydang1412@gmail.com>
2021-09-17 11:04:48 +0700

    finish edit product info

--- AutoWorld/Content/Library/scripts/InnerDescription.js
+++ AutoWorld/Content/Library/scripts/InnerDescription.js
@@ -1,8 +1,12 @@
 ﻿var DetailDescription = $('#DetailDescription').val();
 var objectDescription = JSON.parse(DetailDescription);
+
 var stringInnerDiscription = '';
 var m = 0;
 var srcImageDetail = '';
+var index = 0;
+var objectDesAfterUpdate = {};
+var arrayDesAfterUpdate = [];
 
 var selectUrlForImage = function (btnselect, txtImg, imgDetail) {
     btnselect.on("click", function (e) {
@@ -22,16 +26,33 @@ jQuery.each(objectDescription, function (k, value) {
     if (k.includes("img")) {
         srcImageDetail = "/Content/Library/" + value; 
         
-        stringInnerDiscription += '<div class="form-group"><label class="control-label col-md-2">' + k + '</label><div class="col-md-10"><input class="form-control" type="text" id="txtImageDetail' + m + '" value="' + value + '" /><a href="#" type="button" class="btn btn-success btn-image" id="btnSelectImageDetail' + m + '">Select</a></div><div class="pt-1 pl-3"><img class="imgSize" id="imgDetail'+m+'" src="' + srcImageDetail+'"/></div></div>';
+        stringInnerDiscription += '<div id="frm'+index+'" class="form-group"><label class="control-label col-md-2">' + k + '</label><div class="col-md-10"><input class="form-control" type="text" id="txtImageDetail' + m + '" value="' + value + '" /><a href="#" type="button" class="btn btn-success btn-image" id="btnSelectImageDetail' + m + '">Select</a></div><div class="pt-1 pl-3"><img class="imgSize" id="imgDetail'+m+'" src="' + srcImageDetail+'"/></div></div>';
         m++;
+        index++;
 
     } else {
-        stringInnerDiscription += '<div class="form-group"><label class="control-label col-md-2">' + k + '</label><div class="col-md-10"><input class="form-control" type="text" value="' + value + '" /></div></div>'
+        stringInnerDiscription += '<div id="frm'+index+'" class="form-group"><label class="control-label col-md-2">' + k + '</label><div class="col-md-10"><input class="form-control" type="text" value="' + value + '" /></div></div>'
+        index++;
     }
 
     
 });
 
+$('#btnSave').click(function () {
+    
+    arrayDesAfterUpdate = [];
+    for (var i = 0; i < index; i++) {
+        var formSelectorId = '#frm' + i + ' > label';
+        var formSelectorVal = '#frm' + i + ' :input';
+        var idDes = $(formSelectorId).text();
+        var valDes = $(formSelectorVal).val();
+        var item = [idDes, valDes];
+        arrayDesAfterUpdate.push(item);
+        
+    }
+    objectDesAfterUpdate = Object.fromEntries(arrayDesAfterUpdate);
+    $('#DetailDescription').val(JSON.stringify(objectDesAfterUpdate));
+});
 
 //var ChangeImage = function (i) {
 //    var i = 0;
